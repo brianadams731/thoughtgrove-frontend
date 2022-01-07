c57bcda16bba09b2c6f1fef3e728f4d721c243aa
brianadams731 <brianadams731@gmail.com>
2022-01-06 18:43:36 -0800

    Added button animation and logo hover animation

--- src/components/RegisterCard.tsx
+++ src/components/RegisterCard.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styles from "../styles/RegisterCard.module.css";
 import { Logo } from "../svg/Logo";
 import { CardFormInput } from "./CardFormInput";
