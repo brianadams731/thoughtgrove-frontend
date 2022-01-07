bd4fc64dc6704354e8152233fc88e8fccd1dc764
brianadams731 <brianadams731@gmail.com>
2022-01-06 17:20:31 -0800

    Added basic routing

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,16 +1,23 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Landing } from "./Landing";
+import { Register } from "./Register";
+import { Login } from "./Login";
 import { Footer } from "../components/Footer";
 import styles from "../styles/App.module.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className={styles.wrapper}>
-        <Landing />
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <div className={styles.wrapper}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+      
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/pages/Login.tsx
@@ -0,0 +1,9 @@
+const Login = ():JSX.Element =>{
+    return (
+        <div>
+            Log In
+        </div>
+    )
+}
+
+export {Login};
