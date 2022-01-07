c0ad3ce267120cf0d35ae4dcaf18c1ca37be1a44
brianadams731 <brianadams731@gmail.com>
2022-01-06 23:26:54 -0800

    Added fix info for collapsing login and reg card

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,6 +4,7 @@ import { PreHeader } from "../components/PreHeader";
 import styles from "../styles/Login.module.css";
 
 const Login = ():JSX.Element =>{
+    // write get bottom y value hook then add it to the padding for log in card!!! 
     return (
         <>
             <PreHeader url="/register" tag="Register" />
