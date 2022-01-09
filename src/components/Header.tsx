f3790f1698aae628319c2ae99a845d525b7d3e8b
brianadams731 <brianadams731@gmail.com>
2022-01-08 17:18:46 -0800

    Added Dashboard Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,85 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+
 import styles from "../styles/Header.module.css";
+import { HeroLogoSmall } from "./HeroLogoSmall";
 
 const Header = () =>{
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const upperLine = {
+        init:{
+            top:"0%",
+            rotate:0,
+            translateY:"-50%",
+            transition:{
+                top:{
+                    delay:.4,
+                },
+            }
+        },
+
+        animate:{
+            top:"50%",
+            rotate:45,
+            translateY:"-50%",
+            transition:{
+                rotate:{
+                    delay:.4,
+                },
+            }
+        },
+    }
+
+    const lowerLine = {
+        init:{
+            bottom:"0%",
+            rotate:0,
+            translateY:"50%",
+            transition:{
+                bottom:{
+                    delay:.4,
+                },
+            }
+        },
+
+        animate:{
+            bottom:"50%",
+            rotate:-45,
+            translateY:"50%",
+            transition:{
+                rotate:{
+                    delay:.4,
+                },
+            }
+        },
+
+    }
+
+    const middleLine = {
+        init:{
+            opacity:1,
+            transition:{
+                delay:.2,
+            }
+        },
+        animate:{
+            opacity:0,
+            transition:{
+                duration:.1,
+                delay:.2,
+            }
+        }
+    }
+
     return (
         <header className={styles.wrapper}>
-
+            <HeroLogoSmall />
+            <div className={styles.menuIcon} onClick={()=>{setMenuOpen(prev => !prev);}}>
+                <motion.div initial="init" animate={menuOpen?"animate":"init"} variants={upperLine} className={styles.lineTop}></motion.div>
+                <motion.div initial="init" animate={menuOpen?"animate":"init"} variants={middleLine} className={styles.lineMiddle}></motion.div>
+                <motion.div initial="init" animate={menuOpen?"animate":"init"} variants={lowerLine} className={styles.lineBottom}></motion.div>
+            </div>
         </header>
     )
 }
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import { Header } from "../components/Header";
 import styles from "../styles/Dashboard.module.css";
 import { Home } from "./Home";
 
@@ -6,6 +7,7 @@ const Dashboard = ():JSX.Element =>{
     
     return (
         <div className={styles.wrapper}>
+            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
                 {/*<Route path="deck/*" element={ } />*/}
