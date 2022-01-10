dcd445ec85b4ea4c05564d265beff536c5d7309c
brianadams731 <brianadams731@gmail.com>
2022-01-09 21:03:58 -0800

    Removed test icons from deck meta data

--- src/components/DeckMetaData.tsx
+++ src/components/DeckMetaData.tsx
@@ -1,8 +1,7 @@
 import styles from "../styles/DeckMetaData.module.css";
 import type { IDeckMetaData } from "../Interfaces/IDeckMetaData";
 import { Logo } from "../svg/Logo";
-import { CorrectIcon } from "../svg/CorrectIcon";
-import { WrongIcon } from "../svg/WrongIcon";
+
 
 const DeckMetaData = ({subject, title}:IDeckMetaData) =>{
     return(
@@ -12,8 +11,6 @@ const DeckMetaData = ({subject, title}:IDeckMetaData) =>{
             <div className={styles.logoWrapper}>
                 <Logo svgWidth="30px" fill={"var(--c-main-gray)"} />
             </div>
-            <CorrectIcon height="50px" fill="var(--c-main-gray)" hoverFill="var(--c-achievement-green)" />
-            <WrongIcon width="50px" fill="var(--c-main-gray)" hoverFill="var(--c-achievement-orange)" />
         </section>
     )
 }
