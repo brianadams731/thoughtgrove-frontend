20fcc6433dbe21b52a752146240bc844cc599c84
brianadams731 <brianadams731@gmail.com>
2022-01-09 21:06:40 -0800

    Added fade option to deck meta data

--- src/components/DeckMetaData.tsx
+++ src/components/DeckMetaData.tsx
@@ -2,12 +2,15 @@ import styles from "../styles/DeckMetaData.module.css";
 import type { IDeckMetaData } from "../Interfaces/IDeckMetaData";
 import { Logo } from "../svg/Logo";
 
+interface Props extends IDeckMetaData{
+    fade?:boolean;
+}
 
-const DeckMetaData = ({subject, title}:IDeckMetaData) =>{
+const DeckMetaData = ({subject, title, fade}:Props) =>{
     return(
         <section className={styles.wrapper}>
             <h3 className={styles.subject}>{subject}</h3>
-            <h2 className={styles.title}>{title}</h2>
+            <h2 style={fade?{color:"var(--c-main-gray)", fontWeight:"400"}:{}} className={styles.title}>{title}</h2>
             <div className={styles.logoWrapper}>
                 <Logo svgWidth="30px" fill={"var(--c-main-gray)"} />
             </div>
